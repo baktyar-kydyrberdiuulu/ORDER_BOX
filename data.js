799e833f03fd6cc34de0c044f06542b93f73b74a
baktyar-kydyrberdiuulu <76588630+baktyar-kydyrberdiuulu@users.noreply.github.com>
2021-02-13 10:17:34 +0600

    Einfach code über food menu

---
+++ data.js
@@ -0,0 +1,40 @@
+const menu_items = [{
+    title: 'Hamburger',
+    price: 80,
+    img: './images/hamburger.png',
+    id:1
+},
+{
+    title: 'Coffee',
+    price: 100,
+    img: './images/coffee.png',
+    id:2
+},
+{
+    title: 'Cola',
+    price: 60,
+    img: './images/cola.svg',
+    id:3
+
+}, 
+{
+    title: 'Tea',
+    price: 50,
+    img: './images/tea.svg',
+    id:4
+},
+{
+    title: 'Cheeseburger',
+    price: 100,
+    img: './images/cheeseburger.svg',
+    id:5
+},
+{
+    title: 'Fries',
+    price: 40,
+    img: './images/fries.svg',
+    id:6
+}
+]
+
+let orders_basket = []
